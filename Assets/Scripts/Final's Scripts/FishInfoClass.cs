9c09e5d893103b8076ac089287e69c6cb4b0636d
2012003073 <kyungse1171@naver.com>
2017-11-20 16:59:41 +0900

    edit more

--- Assets/Scripts/Final's Scripts/FishInfoClass.cs
+++ Assets/Scripts/Final's Scripts/FishInfoClass.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class FishInfoClass : MonoBehaviour {
-	public string[] Exp = { "Name: Red\n\nIt lives mainly in the Pacific Ocean and \nhas an average life expectancy of 10 years.",
-							"Name: Yellow04\n\nIt lives mainly in the Pacific Ocean and \nhas an average life expectancy of 10 years.",
-							"Name: Yellow05\n\nIt lives mainly in the Pacific Ocean and \nhas an average life expectancy of 10 years.",
-							"Name: Yellow07\n\nIt lives mainly in the Pacific Ocean and \nhas an average life expectancy of 10 years.",
-							"Name: Yellow09\n\nIt lives mainly in the Pacific Ocean and \nhas an average life expectancy of 10 years." };
+	public string[] Exp = { "Name: Angel Fish\n\nIt is mainly distributed in the Amazon River \nand its tributaries. Also, it has \nan average life expectancy of 5 years.",
+							"Name: Double saddle back butterfly\n\nIt lives mainly in the Indian Ocean and \nhas an average life expectancy of 3 years.",
+							"Name: Beta\n\n\nIt's length of the body is about 5cm in height\nand one of the aquarium fishes",
+							"Name: Cichlid\n\nIt mostly lives in Africa, Central America, and South America.\nBecause of the variety of beautiful colors,\nit is often used as an ornamental fish.",
+							"Name: Emperor angel fish\n\nIn childhood it is a dark blue body with a round white, blue pattern,\nIt becomes a colorful color with a yellow and bright orange pattern as it becomes an adult." };
 
 	// Use this for initialization
 	void Start () {
