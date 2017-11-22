757b1e0f0ecd0e7fc036e495c6a980abd4b7c682
2012003073 <kyungse1171@naver.com>
2017-11-22 17:01:14 +0900

    add intro scene

---
+++ Assets/Scripts/Final's Scripts/temp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class temp : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		Invoke ("SetSound", 3.5f);
+		Invoke ("MoveToGameScene", 10.0f);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SetSound() {
+		GameObject.FindGameObjectWithTag ("movie").GetComponent<AudioSource>().Play();
+	}
+
+	private void MoveToGameScene() {
+		SceneManager.LoadScene ("Final");
+	}
+}
